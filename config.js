85dfe30a1db809afbb390c537e8d729a9d8fda2a
17hv <93518927+17hv@users.noreply.github.com>
2021-11-02 07:49:32 +0700

    Create config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Emm';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc mừng em iu đã đỗ Myeongji Dae';
 const titleCard = 'Tặng em pé Halinn ';
-const contentCard = 'Chúc mừng em đã xuất sắc đạt được ước mơ của mình! CHúc bae của anh sẽ tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những tháng sau này ở Hàn Quốc';
+const contentCard = 'Chúc mừng em đã xuất sắc đạt được ước mơ của mình. CHúc bae của anh sẽ tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những tháng sau này ở Hàn Quốc';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
